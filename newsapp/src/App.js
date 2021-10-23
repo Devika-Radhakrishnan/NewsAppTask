b35443434444bafbf40d2a6bf816ad28a11065d2
devika <devika.radhakrishnan.g@gmail.com>
2021-10-23 13:34:45 +0530

    React app created and commponents created

---
+++ newsapp/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import NewsData from './components/NewsData';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+    <NavBar/>
+    <News/>
+    <NewsData/>
+      </div>
+    )
+  }
+}
+
