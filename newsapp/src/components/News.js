746fa1035bc5f5faf5a1db0c039eb13da8dfba64
devika <devika.radhakrishnan.g@gmail.com>
2021-10-23 22:42:50 +0530

    git button functionality added for prev

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -2,22 +2,44 @@ import React, { Component } from 'react'
 import NewsData from './NewsData'
 
 export class News extends Component {
-  
+
     constructor() {
         super();
         this.state = {
             articles: [],
             loading: false,
-            page:1
+            page: 1
 
         }
     }
-    async componentDidMount(){
-        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=9429ae74542a401197284e80a483e9f9&page=1";
-        let data=await fetch(url);
-        let parsedData=await data.json()
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=9429ae74542a401197284e80a483e9f9&page=1&pageSize=15";
+        let data = await fetch(url);
+        let parsedData = await data.json()
         console.log(parsedData);
-        this.setState({articles:parsedData.articles})
+        this.setState({ articles: parsedData.articles })
+    }
+    handlePreviousClick = async () => {
+
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=9429ae74542a401197284e80a483e9f9&page=${this.state.page-1}&pageSize=15`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({})
+        this.setState({
+            page: this.state.page - 1,
+
+        })
+    }
+    handleNextClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=9429ae74542a401197284e80a483e9f9&page=${this.state.page+1}&pageSize=15`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({})
+        this.setState({
+            page: this.state.page + 1,
+        })
     }
     render() {
         return (
@@ -26,15 +48,15 @@ export class News extends Component {
 
                 <div className="row">
                     {this.state.articles.map((element) => {
-                        return <div className="col-md-4"  key={element.url}>
+                        return <div className="col-md-4" key={element.url}>
                             <NewsData title=
-                            {element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,70):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                                {element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 70) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
                     })}
                 </div>
-                <div>
-                <button type="button" class="btn btn-dark">Previous</button>
-                <button type="button" class="btn btn-dark">Next</button>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Previous</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
