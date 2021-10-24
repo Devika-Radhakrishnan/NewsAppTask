ff1fb75858a0217bdd6cddc32e532d752999db56
devika <devika.radhakrishnan.g@gmail.com>
2021-10-24 22:02:53 +0530

    weather api added

--- newsapp/src/components/WeatherLive.js
+++ newsapp/src/components/WeatherLive.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
 
 export class WeatherLive extends Component {
-    componentDidMount(){
-       //popup for allow location and will get the coordinates
-        navigator.geolocation.getCurrentPosition(function(position){
-console.log(position.coords);
-        })
+    state = {
+        latitude: null,
+        longitude: null,
+        temp:null
+    }
+    //popup for allow location and will get the coordinates
+    componentDidMount() {
+        if (navigator.geolocation) {
+            this.getPositions()
+                .then((position) => {
 
+                    this.getWeather(position.coords.latitude, position.coords.latitude);
+                    // console.log(position.coords.latitude);
+                    // latitude:9.9130729,longitude:10.062265
+                })
+        }
+    }
+    getPositions = () => {
+        return new Promise(function (resolve, reject) {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+    };
+
+    getWeather = async (latitude, longitude) => {
+        const api =await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=017a22617c85c751218b484a4f6d61eb`
+        );
+        const data = await api.json();
+        console.log(data);
+     
     }
     render() {
         return (
